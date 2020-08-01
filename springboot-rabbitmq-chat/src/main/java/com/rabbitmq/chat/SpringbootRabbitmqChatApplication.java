7c1ec1ef2bdf9139af8eaa4479bff9ba34dc4da5
nfebrian13 <nanaz.febrian@gmail.com>
2020-08-01 10:45:27 +0700

    springboot rabbitmq chat

---
+++ springboot-rabbitmq-chat/src/main/java/com/rabbitmq/chat/SpringbootRabbitmqChatApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRabbitmqChatApplication.class, args);
+	}
+
+}
