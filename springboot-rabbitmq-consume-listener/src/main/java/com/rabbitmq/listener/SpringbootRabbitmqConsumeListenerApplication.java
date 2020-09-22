c11321160ba01a7aa5c2729a030bfab3e0abe004
nfebrian13 <nanaz.febrian@gmail.com>
2020-09-22 22:35:15 +0700

    consume listener rabbit onMessage

---
+++ springboot-rabbitmq-consume-listener/src/main/java/com/rabbitmq/listener/SpringbootRabbitmqConsumeListenerApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqConsumeListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRabbitmqConsumeListenerApplication.class, args);
+	}
+
+}
